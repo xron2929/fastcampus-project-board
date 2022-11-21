adfebea399d4cea4905c6b48b9a5423a699a69cf
consistent <xron2929@gmail.com>
2022-11-21 21:09:17 +0900

    도메인 클래스 설계
    관련 주석 발행

--- fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/domain/ArticleComment.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/domain/ArticleComment.java
@@ -4,11 +4,11 @@ import java.time.LocalDateTime;
 
 public class ArticleComment {
     private Long id;
-    private Article article;
-    private String content;
+    private Article article;    // 제목
+    private String content;     // 본문
 
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    private LocalDateTime createdAt;    // 생성일자
+    private String createdBy;   // 생성자
+    private LocalDateTime modifiedAt;   // 수정일자
+    private String modifiedBy;  // 수정자
 }
