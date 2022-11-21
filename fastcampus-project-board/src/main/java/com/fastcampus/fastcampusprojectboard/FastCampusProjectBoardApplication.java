cf62775ecd2ecc785fb465e5c630e95260fbdf78
consistent <xron2929@gmail.com>
2022-11-21 20:50:15 +0900

    class name change

--- fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/FastCampusProjectBoardApplication.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/FastCampusProjectBoardApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FastcampusProjectBoardApplication {
+public class FastCampusProjectBoardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FastcampusProjectBoardApplication.class, args);
+		SpringApplication.run(FastCampusProjectBoardApplication.class, args);
 	}
 
 }
