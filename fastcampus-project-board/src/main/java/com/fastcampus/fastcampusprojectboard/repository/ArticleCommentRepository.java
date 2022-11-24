6ef8e66d128a77bb5f4fda6163ad424a557a1c1f
consistent <xron2929@gmail.com>
2022-11-24 18:37:09 +0900

    hal api 등록
    HAL API 조회를 위해 코드 추가

--- fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleCommentRepository.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleCommentRepository.java
@@ -1,9 +1,18 @@
 package com.fastcampus.fastcampusprojectboard.repository;
 
 import com.fastcampus.fastcampusprojectboard.domain.ArticleComment;
+import com.fastcampus.fastcampusprojectboard.domain.QArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+@RepositoryRestResource
 @Repository
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment,Long> {
+public interface ArticleCommentRepository
+        extends JpaRepository<ArticleComment,Long>
+        ,QuerydslPredicateExecutor<ArticleComment>
+        // ,QuerydslBinderCustomizer<QArticleComment>
+{
 }
--- fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleRepository.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleRepository.java
@@ -1,14 +1,26 @@
 package com.fastcampus.fastcampusprojectboard.repository;
 
 import com.fastcampus.fastcampusprojectboard.domain.Article;
+import com.fastcampus.fastcampusprojectboard.domain.QArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+@RepositoryRestResource
 @Repository
 public interface ArticleRepository
-        extends JpaRepository<Article,Long>,
-        QuerydslPredicateExecutor<Article>
+        extends JpaRepository<Article,Long>
+        ,QuerydslPredicateExecutor<Article>
+        // ,QuerydslBinderCustomizer<QArticle>
+    // 여기 위에 하나는 Article 하나는 QArticle인데 predi는  기본 정보
+        // Blinder는 Qclass 정보라는 걸 꼭 맞춰야 됨
+    // 갖다 대보면 확인 할수있는데 Prdicate는 T 일반 정보가 들어가고
+    // Blinder는 Entitypath<?> 이런 식으로 Q엔티티 관련 정보가 필요하다는거
+    // 확인 O
+    // QuerydslPredicate만 있어도 검색 기능 만들 수 있어서
+    // 따로 만들지 않음 여기서 하는 말은
+
 {
 }
