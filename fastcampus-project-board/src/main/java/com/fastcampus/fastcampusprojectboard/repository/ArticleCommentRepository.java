dc2b493407f3c93a39f0b980bf4020b2af64fedf
consistent <xron2929@gmail.com>
2022-11-24 17:45:10 +0900

    Queryds 의존성 관리2
    마찬가지로 Querydsl 관리를 위해 계속 의존성을 추가하는 등 여러작업을 진행하고 있습니다.

--- fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleCommentRepository.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.fastcampus.fastcampusprojectboard.repository;
 
 import com.fastcampus.fastcampusprojectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment,Long> {
 }
--- fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleRepository.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleRepository.java
@@ -2,6 +2,13 @@ package com.fastcampus.fastcampusprojectboard.repository;
 
 import com.fastcampus.fastcampusprojectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.stereotype.Repository;
 
-public interface ArticleRepository extends JpaRepository<Article,Long> {
+@Repository
+public interface ArticleRepository
+        extends JpaRepository<Article,Long>,
+        QuerydslPredicateExecutor<Article>
+{
 }
