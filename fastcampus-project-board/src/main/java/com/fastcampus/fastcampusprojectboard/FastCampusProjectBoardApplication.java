adb5e2538b7462cd77f6ea732843e8e99b4000d0
consistent <xron2929@gmail.com>
2022-11-24 18:49:10 +0900

    hal 관리

--- fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/FastCampusProjectBoardApplication.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/FastCampusProjectBoardApplication.java
@@ -1,13 +1,42 @@
 package com.fastcampus.fastcampusprojectboard;
 
+import com.fastcampus.fastcampusprojectboard.domain.Article;
+import com.fastcampus.fastcampusprojectboard.domain.ArticleComment;
+import com.fastcampus.fastcampusprojectboard.random.RandomString;
+import com.fastcampus.fastcampusprojectboard.repository.ArticleCommentRepository;
+import com.fastcampus.fastcampusprojectboard.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.Random;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class FastCampusProjectBoardApplication {
-
+	@Autowired ArticleRepository articleRepository;
+	@Autowired
+	ArticleCommentRepository articleCommentRepository;
+	@PostConstruct
+	void sdf() {
+		long maxLongArticleId = 100;
+		Random random = new Random();
+		RandomString randomString = new RandomString();
+		for(int i = 0; i < maxLongArticleId; i++) {
+			String title = randomString.randomSizeString(20);
+			String content = randomString.randomSizeString(20);
+			String hashtag = randomString.randomSizeString(20);
+			Article article = Article.of(title, content, hashtag);
+			articleRepository.save(article);
+			Long articleId = random.nextLong(maxLongArticleId);
+			Article findArticle = articleRepository.findById(articleId).orElse(null);
+			String content2 = randomString.randomSizeString(20);
+			ArticleComment articleComment = ArticleComment.of(findArticle,content2);
+			articleCommentRepository.save(articleComment);
+		}
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FastCampusProjectBoardApplication.class, args);
 
