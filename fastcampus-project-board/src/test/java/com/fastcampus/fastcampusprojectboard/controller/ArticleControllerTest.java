d65e89e921dcc8cf92e7db37003a48753dc2181b
consistent <xron2929@gmail.com>
2022-11-26 19:59:25 +0900

    간단한 뷰 테스트
    앞서 간단히 만들었던 컨트롤러를 테스트 작업을 통해 품질 보증 작업

--- fastcampus-project-board/src/test/java/com/fastcampus/fastcampusprojectboard/controller/ArticleControllerTest.java
+++ fastcampus-project-board/src/test/java/com/fastcampus/fastcampusprojectboard/controller/ArticleControllerTest.java
@@ -2,8 +2,11 @@ package com.fastcampus.fastcampusprojectboard.controller;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -13,9 +16,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 // CI CD 배포는 기본적으로 빌드가 성공해야 커밋이 되게 할 텐데
 // 이걸 막으려고 @Disabled로 무시해서 빌드하도록 해서 해결함
-@Disabled("Controller를 만들지 않아서 무시하도록 처리")
-@WebMvcTest(ArticleController.class)
-// 의존성 클래스를 이렇게 직접 추가 하면 하나만 읽어들여서 태스트 함
+// @Disabled("Controller를 만들지 않아서 무시하도록 처리")
+// @WebMvcTest(ArticleController.class)
+// 의존성 클래스를 이렇게 직접 추가 하면 하나만 읽어들여서 태스트
+@AutoConfigureMockMvc
+@SpringBootTest
 class ArticleControllerTest {
     private final MockMvc mvc;
 
@@ -24,20 +29,22 @@ class ArticleControllerTest {
         this.mvc = mockMvc;
     }
     @DisplayName("[view] [GET] 게시글 리스트 [게시판] 페이지 - 정상 호출")
+    @Test
     public void givenNothing_wheRequestArticlesView_thanReturnsArticleView() throws Exception {
         //given
 
         //when & then
         mvc.perform(get("/articles"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(view().name("aritlces/index"))
-                .andExpect(model().attributeExists("articles"))
-                .andExpect(model().attributeExists("articleComments"));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/main-index"))
+                .andExpect(model().attributeExists("articles"));
         // model로 aritcles랑 COmments 정보가 있어야되고 기본 적으로 이동하는 상세페이지가 detail이라는 점
-
+        // contentType은 해당 타입만 허용인데, Text는 Text인데 UTF-8이 들어오고, 나라마다 호환하는 텍스트 파일이 다를 수 있으니
+        // 호환이 되는 종류 중 하나라고 contentTypeCompatibleWith 라고 함
 
     }
     @DisplayName("[view] [GET] 게시글 상세 [게시판] 페이지 - 정상 호출")
+    @Test
     public void givenNothing_wheRequestArticleView_thanReturnArticleView() throws Exception {
         //given
 
