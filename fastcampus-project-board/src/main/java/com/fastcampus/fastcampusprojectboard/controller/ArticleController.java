9ec682717b777a7112df5eaa17e39e0709500a4c
consistent <xron2929@gmail.com>
2022-11-26 19:59:06 +0900

    컨트롤러 생성
    뷰 인식이 되는지  컨트롤러를 생성하고, 간단히 웹 페이지 생성

--- fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/controller/ArticleController.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/controller/ArticleController.java
@@ -1,10 +1,21 @@
 package com.fastcampus.fastcampusprojectboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RequestMapping("/articles")
+import java.util.List;
+
+@RequestMapping
 @Controller
 public class ArticleController {
+    @GetMapping("/articles")
+    public String article(ModelMap map) {
+        map.addAttribute("articles", List.of());
+        return "articles/main-index";
+    }
+
 
 }
