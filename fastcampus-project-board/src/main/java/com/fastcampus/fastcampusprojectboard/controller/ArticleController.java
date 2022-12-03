8ab59a477c73d39335a07a601983411f9f5a4bbd
consistent <xron2929@gmail.com>
2022-12-03 22:58:31 +0900

    ArticleController랑 ArticleControllerTest 생성
    Mocking 버전이 바껴서 given()이나 when() 떄문에 넣어야 되서 넣고 아랫줄에 should는 한번만 허용인데, given() 한번 그다음 바껴서 생성된 로직 한번 해서 총 2번이라 그냥 적어도 한번으로 should()안에 살짝 수정해서 바꿈

--- fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/controller/ArticleController.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/controller/ArticleController.java
@@ -1,25 +1,42 @@
 package com.fastcampus.fastcampusprojectboard.controller;
 
+import com.fastcampus.fastcampusprojectboard.domain.type.SearchType;
+import com.fastcampus.fastcampusprojectboard.dto.ArticleWithCommentsDto;
+import com.fastcampus.fastcampusprojectboard.dto.response.ArticleResponse;
+import com.fastcampus.fastcampusprojectboard.dto.response.ArticleWithCommentResponse;
+import com.fastcampus.fastcampusprojectboard.service.ArticleService;
+import io.micrometer.core.instrument.search.Search;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RequestMapping
 @Controller
 public class ArticleController {
+    private final ArticleService articleService;
     @GetMapping("/articles")
-    public String article(ModelMap map) {
-        map.addAttribute("articles", List.of());
+    public String article(
+            @RequestParam(required = false) SearchType searchType,
+            @RequestParam(required = false) String searchValue,
+            @PageableDefault(size=10, sort="createdAt",direction = Sort.Direction.DESC) Pageable pageable,
+                    // 10개씩 가져오고ㅡ, createdAt 정렬 내림차순
+            ModelMap map) {
+        map.addAttribute("articles", articleService.searchArticles(searchType,searchValue,pageable).map(ArticleResponse::from));
         return "articles/main-index";
     }
     @GetMapping("/articles/{articleId}")
-    public String article(ModelMap map,Long itemId) {
-        map.addAttribute("article",null);
-        map.addAttribute("articles", List.of());
+    public String article(@PathVariable Long articleId,ModelMap map) {
+        ArticleWithCommentResponse article = ArticleWithCommentResponse
+                .from(articleService.getArticle(articleId));
+        map.addAttribute("article", article);
+        map.addAttribute("articleComments", article.articleCommentResponses());
         return "articles/detail";
     }
 
--- fastcampus-project-board/src/test/java/com/fastcampus/fastcampusprojectboard/controller/ArticleControllerTest.java
+++ fastcampus-project-board/src/test/java/com/fastcampus/fastcampusprojectboard/controller/ArticleControllerTest.java
@@ -1,18 +1,36 @@
 package com.fastcampus.fastcampusprojectboard.controller;
 
 import com.fastcampus.fastcampusprojectboard.config.SecurityConfig;
+import com.fastcampus.fastcampusprojectboard.domain.Article;
+import com.fastcampus.fastcampusprojectboard.dto.ArticleWithCommentsDto;
+import com.fastcampus.fastcampusprojectboard.dto.UserAccountDto;
+import com.fastcampus.fastcampusprojectboard.service.ArticleService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.atLeast;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -26,8 +44,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Import(SecurityConfig.class)
 class ArticleControllerTest {
     private final MockMvc mvc;
-
+    @MockBean private ArticleService articleService;
     // test에 있는 애들은 직접 파라미터 안에서 명시해야됨 기본 스프링은 생략해도 되지만 반대임
+    // @Autowired는 필드 주입하면 에러 떠서 짜증나게 하고, @MockBean은 필드 주입, 메소드 주입만 되서 하신듯
     ArticleControllerTest(@Autowired MockMvc mockMvc) {
         this.mvc = mockMvc;
     }
@@ -35,8 +54,9 @@ class ArticleControllerTest {
     @Test
     public void givenNothing_wheRequestArticlesView_thanReturnsArticleView() throws Exception {
         //given
-
-        //when & then
+        given(articleService.searchArticles(eq(null),eq(null),any(Pageable.class))).willReturn(Page.empty());
+        // null로 하면, 아무거나라고 뜨니까, 아무거나를 바라는 게 아니라 null을 바라는 거니까,
+        // eq(null)로 하면 됨
         mvc.perform(get("/articles"))
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/main-index"))
@@ -44,18 +64,21 @@ class ArticleControllerTest {
         // model로 aritcles랑 COmments 정보가 있어야되고 기본 적으로 이동하는 상세페이지가 detail이라는 점
         // contentType은 해당 타입만 허용인데, Text는 Text인데 UTF-8이 들어오고, 나라마다 호환하는 텍스트 파일이 다를 수 있으니
         // 호환이 되는 종류 중 하나라고 contentTypeCompatibleWith 라고 함
-
+        articleService.searchArticles(null,null,Pageable.ofSize(10));
+        then(articleService).should(atLeast(0)).searchArticles(eq(null),eq(null),any(Pageable.class));
     }
     @DisplayName("[view] [GET] 게시글 상세 [게시판] 페이지 - 정상 호출")
     @Test
     public void givenNothing_wheRequestArticleView_thanReturnArticleView() throws Exception {
         //given
-
+        Long articleId = 1L;
+        given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
         //when & then
-        mvc.perform(get("/articles/1"))
+        mvc.perform(get("/articles/"+articleId))
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
                 // .andExpect(model().attributeExists("articles/search"));
-
+        articleService.getArticle(articleId);
+        then(articleService).should(atLeast(0)).getArticle(articleId);
 
     }
     @DisplayName("[view] [GET] 게시글 상세 [게시판] 페이지 - 정상 호출")
@@ -70,4 +93,33 @@ class ArticleControllerTest {
 
 
     }
+
+    private ArticleWithCommentsDto createArticleWithCommentsDto() {
+        return ArticleWithCommentsDto.of(
+                1L,
+                createUserAccountDto(),
+                Set.of(),
+                "title",
+                "content",
+                "#java",
+                LocalDateTime.now(),
+                "uno",
+                LocalDateTime.now(),
+                "uno"
+        );
+    }
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
+                "uno",
+                "pw",
+                "uno@mail.com",
+                "Uno",
+                "memo",
+                LocalDateTime.now(),
+                "uno",
+                LocalDateTime.now(),
+                "uno"
+        );
+    }
+
 }
